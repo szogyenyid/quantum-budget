05ed2009cc98ad808dc95f4a72531074687de189
soegaeni <szogyenyid@gmail.com>
2021-08-09 22:13:51 +0200

    Add dummy database data

--- js/QuantumLoader.js
+++ js/QuantumLoader.js
@@ -5,11 +5,23 @@ class QuantumLoader{
         if(debug) console.log("QuantumLoader is instantiated.");
 
         // Read data from Database
-        dummy = {
-            
+        this.dummy = {
+            "Cat1": {
+                "Item1": [1400, 1000],
+                "Item2": [0, 0],
+                "Item3": [250000, 264123]
+            },
+            "Category 2":{
+                "First item": [100, 80],
+                "Second item": [800, 120]
+            },
+            "Third category":{
+
+            }
         }
 
         // Load templates
+        // Later on, the whole DB should be passed as a constructor parameter
         this.codeGenerator = new CodeGenerator();
         console.log((this.codeGenerator == undefined ? "CodeGenerator failed to instantiate" : "CodeGenerator instantiated"));
 
