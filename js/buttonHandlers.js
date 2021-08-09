dce57f8d09c989ada4b7d3677bcb04ff2edb6be3
soegaeni <szogyenyid@gmail.com>
2021-08-09 20:10:23 +0200

    Add eventListener on save button

--- js/buttonHandlers.js
+++ js/buttonHandlers.js
@@ -1,6 +1,10 @@
 // Budget -> Settings
 var budgetSettingsModal = new BulmaModal("#budgetSettings")
-document.querySelector("#settingsIcon").addEventListener("click", function (){
+document.querySelector("#settingsIcon").addEventListener("click", function (e){
 	budgetSettingsModal.show()
 })
 
+// Budget -> Settings -> Save
+document.querySelector("#saveBudgetSettings").addEventListener("click",function(e){
+	console.log("Todo: saving settings");
+})
